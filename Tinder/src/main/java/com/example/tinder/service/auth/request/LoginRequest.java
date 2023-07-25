9a2109aa61f57263a21df90ce871d25d81257b79
DangggKhoaaa <khoaduong.27011998@gmail.com>
2023-07-25 11:02:21 +0700

    khoa 25/07 11h02

---
+++ Tinder/src/main/java/com/example/tinder/service/auth/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.example.tinder.service.auth.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
